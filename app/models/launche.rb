fe8b21b8f317f438da00a5d6fa6e6c6205e04d59
igorcb <igor.batista@gmail.com>
2013-04-08 17:43:38 -0300

    adicionado os metodos para pegar os totai por ano e por mes

--- app/models/launche.rb
+++ app/models/launche.rb
@@ -3,4 +3,31 @@ class Launche < ActiveRecord::Base
 
   validates :date_launche, :price, :total, presence: true
   validates :miles, presence: true, numericality: true
+
+  scope :by_year, lambda { |year| where('extract(year from date_launche) = ?', year) }
+
+  def self.total_launche_year(year)
+    Launche.where('extract(year from date_launche) = ?', year).sum('total')
+  end
+
+  def self.total_launche_year_month(year)
+    Launche.where('extract (year FROM date_launche) = ?', year).group('extract (month FROM date_launche)').sum('total').sort
+  end
+
+  def self.miles_travel_year(year)
+  	miles_first = Launche.where('extract (year FROM date_launche) = ?', year).order('miles').first
+  	miles_last  = Launche.where('extract (year FROM date_launche) = ?', year).order('miles').last
+
+  	result = (miles_last.miles.to_i - miles_first.miles.to_i)
+  	result
+  end
+
+  def self.miles_travel_year_and_month(year, month)
+    miles_first = Launche.where('extract (year FROM date_launche) = ? and extract (month FROM date_launche) = ? ', year, month).order('miles').first
+    miles_last  = Launche.where('extract (year FROM date_launche) = ? and extract (month FROM date_launche) = ? ', year, month).order('miles').last
+
+    result = (miles_last.miles.to_i - miles_first.miles.to_i)
+    result
+  end
+
 end
